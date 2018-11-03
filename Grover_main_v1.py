9e5158a690efe06efc722096065dd80886ae4e3a
Grant Otto <gotto@udel.edu>
2018-11-03 15:07:37 -0400

    added main loop in Grover main,  added whiteboard coding image from a while back

--- Grover_main_v1.py
+++ Grover_main_v1.py
@@ -1,12 +1,57 @@
 #! /usr/bin/python
 
+'''
+Grover: a grain size-measuring rover using Pixhawk 2.0 connected via a serial connecti$
+
+Developed by Grant Otto, Courtney Cowger, Zach El-Azom, Eriq Gloria, Cole Stinger, Aar$
+University of Delaware Department of Mechanical Engineering
+Faculty Advisor: Adam Wickenheiser
+
+Developed for use at the Robotics Discovery Lab, University of Delaware School of Mari$
+Sponsor and PI of Robotics Discovery Lab: Arthur Trembanis
+
+wwww.udel.edu
+
+All code is public and free to use.
+'''
+
+'''
+***** KNOWN ISSUES *****
+- take image function needs to add in all code for taking the image, gps point, stepper control, etc
+- if the switch for the image capture is left down, the vehicle will continuously take image after image.
+	In the future, the r/c channel will be changed to a button but this will require a bit more troubleshooting
+	on the controller.
+
+'''
+
+
+
+import time
 from dronekit import *
 '''from dronekit import connect'''
 vehicle = connect('/dev/ttyS0', wait_ready=True, baud=921600)
 print("Hello, my name is Grover. The current firmware version is: ")
 print vehicle.version
 
+while True: 							# starts a perpetual loop any time the vehicle is connected
+	while vehicle.mode==VehicleMode('AUTO'): 		# if the vehicle is in auto
+		dist=distance_to_current_waypoint()
+		if dist<1 and not dist == None: 		# if the vehicle is less than a meter away from the current WP
+			vehicle.mode = VehicleMode('HOLD') 	# put it on hold (on a rover, will stop the vehicle)
+			time.sleep(3) 				# wait for the vehicle to come to a stop (3 seconds)
+			take_image()
+			vehicle.mode = VehicleMode('AUTO') 	# put it back in AUTO
+			time.sleep(15) 				# wait for 15 seconds so the vehicle can exit
+	while vehicle.mode==VehicleMode('MANUAL'): 		# if the vehicle is in MANUAL (remotely operated) mode:
+		if rc.channel('6') > 1750 			# if the switch by the H button on the Lightbridge is lowered
+			vehicle.mode = VehicleMode('HOLD') 	# put the vehicle in HOLD (on a rover, will stop the vehicle)
+			time.sleep(3) 				# wait for the vehicle to come to a stop
+			take_image()
+			vehicle.mode = VehicleMode('MANUAL')
+								# ***make sure the switch is immediately put back up after turning down
 
+			
+		
 
 
 
@@ -20,8 +65,21 @@ def take_image():
 	'''
 	controls the stepper motor going down, the image being taken,
 	and the stepper coming back up
-	"""
-	
+	'''
+
+
+	'''
+	from whiteboard code:
+	while switch = LOW
+                lower stepper
+        stop stepper
+        retrieve gps point
+        take usb image
+        modify exif tag to include gps point
+        save to directory
+        raise stepper #may need a switch for the top of the stepper
+
+	'''
 
 
 
