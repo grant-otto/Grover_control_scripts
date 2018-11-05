4e113638b7dd6702dbec28bae40e61ff96bac4d5
cacowger <43860806+cacowger@users.noreply.github.com>
2018-11-05 11:58:24 -0500

    Add files via upload
    Camera Capture and Exif Tag Modification

---
+++ Main.py
@@ -0,0 +1,43 @@
+import numpy as np
+import cv2
+import os
+import pyexiv2
+from datetime import date
+from datetime import time
+from datetime import datetime
+from datetime import tzinfo
+from dronekit import connect, VehicleMode
+
+def main():
+    cap = cv2.VideoCapture(0) #zero is first webcam attatched
+    n = 0
+    now = datetime.now()
+    
+    if cap.isOpened(): #checks if webcam is up and running
+        ret, frame = cap.read() #capture single frame from the webcam to read
+    else:
+        ret = False
+
+    if ret:
+        #GPS Exif Tag Description: A pointer to the GPS Info IFD. The interoperability
+            #structure of the GPS Info IFD, like that of Exif IFD, has no image data
+        #Need to install pyexiv package
+        #pyexiv2 is a module that allows your python scripts to read and write data
+            #embedded in image files
+        metadata = pyexiv2.ImageMetadata(frame) #calls for the metadata off of the image
+        metadata.read() #reads the metadata
+        key = 'Exif.Image.GPSTag' #reference for saving the gps data in the exif tag
+        value = dronekit.LocationGlobal #takes the gps data from dronekit
+        metadata[key] = pyexiv2.ExifTag(key, value) #writes the key and value to the exif tag
+        #%m_%d_%Y - month_day_year
+        #%n - image number
+        filename = "GrainImage_" + % datetime.datetime.now().strftime("%m_%d_%Y %H:%M:%S") + ".jpg" %n
+        path = '/home/pi/GrainSizeImages'
+        cv2.imwrite(os.path.join(path, filename), frame)
+        #cv2.imwrite("GrainImage.jpg", frame) #writes the frame to an image file
+        n+=1
+        
+    cap.release() #relases the camera function
+
+if _name_ == "_main_":
+    main()
