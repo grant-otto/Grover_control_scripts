518bef61b11594b308d451651d35b31b63fb7c49
Grant Otto <gotto@udel.edu>
2018-11-06 13:54:21 -0500

    working on debugging Main.py

--- Main.py
+++ Main.py
@@ -10,32 +10,33 @@ from datetime import tzinfo
 from dronekit import connect, VehicleMode
 
 def main():
-    cap = cv2.VideoCapture(0) #zero is first webcam attatched
+    cap = cv2.VideoCapture(0) 							#zero is first webcam attatched
     n = 0
     now = datetime.now()
     
-    if cap.isOpened(): #checks if webcam is up and running
-        ret, frame = cap.read() #capture single frame from the webcam to read
+    if cap.isOpened(): 								#checks if webcam is up and running
+        ret, frame = cap.read() 						#capture single frame from the webcam to read
     else:
         ret = False
 
     if ret:
-        #GPS Exif Tag Description: A pointer to the GPS Info IFD. The interoperability
-            #structure of the GPS Info IFD, like that of Exif IFD, has no image data
-        #Need to install pyexiv package
-        #pyexiv2 is a module that allows your python scripts to read and write data
-            #embedded in image files
-        metadata = pyexiv2.ImageMetadata(frame) #calls for the metadata off of the image
-        metadata.read() #reads the metadata
-        key = 'Exif.Image.GPSTag' #reference for saving the gps data in the exif tag
-        value = dronekit.LocationGlobal #takes the gps data from dronekit
-        metadata[key] = pyexiv2.ExifTag(key, value) #writes the key and value to the exif tag
+        '''GPS Exif Tag Description: A pointer to the GPS Info IFD. The interoperability
+            structure of the GPS Info IFD, like that of Exif IFD, has no image data
+        Need to install pyexiv package
+        pyexiv2 is a module that allows your python scripts to read and write data
+            embedded in image files
+	'''
+        metadata = pyexiv2.ImageMetadata(frame) 				#calls for the metadata off of the image
+        metadata.read() 							#reads the metadata
+        key = 'Exif.Image.GPSTag' 						#reference for saving the gps data in the exif tag
+        value = dronekit.LocationGlobal 					#takes the gps data from dronekit
+        metadata[key] = pyexiv2.ExifTag(key, value) 				#writes the key and value to the exif tag
         #%m_%d_%Y - month_day_year
         #%n - image number
         filename = "GrainImage_" + datetime.datetime.now().strftime("%m_%d_%Y %H:%M:%S") + ".jpg" %n
         path = '/home/grant/GrainSizeImages'
-        #cv2.imwrite(os.path.join(path, filename), frame)
-        cv2.imwrite(filename, frame) #writes the frame to an image file
+        cv2.imwrite(os.path.join(path, filename), frame)
+        #cv2.imwrite(filename, frame) #writes the frame to an image file
         n+=1
         
     cap.release() #relases the camera function
