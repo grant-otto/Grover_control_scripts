9420a330b38804cb135f6f0536df6e7961268cfc
seaotto <gotto@udel.edu>
2018-11-13 08:47:18 -0500

    started testing gps tag

---
+++ test_funcs/GPSprint_test.py
@@ -0,0 +1,39 @@
+#! /usr/bin/python
+
+'''
+Grover: a grain size-measuring rover using Pixhawk 2.0 connected via a serial connecti$
+
+Developed by Grant Otto, Courtney Cowger, Zach El-Azom, Eriq Gloria, Cole Stinger, Aar$
+University of Delaware Department of Mechanical Engineering
+Faculty Advisor: Adam Wickenheiser
+
+Developed for use at the Robotics Discovery Lab, University of Delaware School of Mari$
+Sponsor and PI of Robotics Discovery Lab: Arthur Trembanis
+
+wwww.udel.edu
+
+All code is public and free to use.
+'''
+
+'''
+***** KNOWN ISSUES *****
+- take image function needs to add in all code for taking the image, gps point, stepper control, etc
+- if the switch for the image capture is left down, the vehicle will continuously take image after image.
+        In the future, the r/c channel will be changed to a button but this will require a bit more troubleshooting
+        on the controller.
+
+'''
+
+import time
+from dronekit import *
+'''from dronekit import connect'''
+vehicle = connect('/dev/ttyS0', wait_ready=True, baud=921600)
+print("Hello, my name is Grover. The current firmware version is: ")
+print vehicle.version
+print vehicle.system_status.state
+print vehicle.armed
+print vehicle.mode
+print 'done checks'
+
+print vehicle.gps_0
+print vehicle.location.global_frame
