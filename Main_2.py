907f1ec11f56021c6ba42404b232071084cdab9f
seaotto <gotto@udel.edu>
2018-11-28 15:17:30 -0500

    issues with Main_2 being called

--- Main_2.py
+++ Main_2.py
@@ -8,39 +8,33 @@ from pathlib import Path
 #import pyexiv2
 import datetime
 #from dronekit import connect, VehicleMode
-#datetime.now().strftime("%m_%d_%Y %H:%M:%S")
+#datetime.now.strftime("%m_%d_%Y %H:%M:%S")
 
 
 def main_2():
-	cap = cv2.VideoCapture(0) 							#zero is first webcam attatched
+	cap = cv2.VideoCapture(0)
 	filename = ''
 
-	if cap.isOpened(): 								#checks if webcam is up and running
-		ret, frame = cap.read() 						#capture single frame from the webcam to read
+	if cap.isOpened():
+		ret, frame = cap.read()
 		#cv2.imshow('frame',frame)
 	else:
 		ret = False
 
 	try:
 		if ret:
-			'''GPS Exif Tag Description: A pointer to the GPS Info IFD. The interoperability
-			structure of the GPS Info IFD, like that of Exif IFD, has no image data
-			Need to install pyexiv package
-			pyexiv2 is a module that allows your python scripts to read and write data
-			embedded in image files
-			'''
 			#%m_%d_%Y - month_day_year
-			#filename = "GrainImage_.png" #+ str(now) + ".png"
-			filename = 'GrainImage_%s'%datetime.datetime.utcnow().strftime('%Y-%m-%d-%H-%M-%S') #writes filename with UTC date & time
-			#path = 'C:/Users/Courtney/Documents/Github/Grover_control_scripts'
+			#filename = "GrainImage.png" #+ str(now) + ".png"
+			filename = 'GrainImage_%s'%datetime.datetime.utcnow().strftime('%Y-%m-%d-%H-%M-%S')
+			#path = 'C:/Users/Courtney/Docunments/Github/Grover_control_scripts'
 			#cv2.imwrite(os.path.join(path, filename), frame)
-			print(filename) #sanity check to make sure filename is storing properly
-			cv2.imwrite(filename, frame) #writes the frame to an image file
+			print(filename)
+			cv2.imwrite(filename, frame)
 			#filename+=new_filename
-		except:
-			pass
+	except:
+		pass
 
-	cap.release() #relases the camera function
+	cap.release()
 
-if __name__ == "__main__":
+if__name__ == "__main__":
 	main_2()
