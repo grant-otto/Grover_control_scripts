0d24809bb326e592b0229d8ace972002a9465388
Grant Otto <gotto@udel.edu>
2018-12-05 18:11:14 -0500

    added while true to mode read test

--- tests/Dronekit_tests/modereadtest.py
+++ tests/Dronekit_tests/modereadtest.py
@@ -6,12 +6,13 @@ vehicle = connect('/dev/ttyS0', wait_ready=True, baud=921600)
 print("Hello, my name is Grover. The current firmware version is: ")
 print vehicle.version
 
-while vehicle.mode==VehicleMode('AUTO'):
-    print('auto')
-    time.sleep(1)
-while vehicle.mode==VehicleMode('MANUAL'):
-    print('manual')
-    time.sleep(1)
-while vehicle.mode==VehicleMode('HOLD'):
-    print('hold')
-    time.sleep(1)
+while True:
+    while vehicle.mode==VehicleMode('AUTO'):
+        print('auto')
+        time.sleep(1)
+    while vehicle.mode==VehicleMode('MANUAL'):
+        print('manual')
+        time.sleep(1)
+    while vehicle.mode==VehicleMode('HOLD'):
+        print('hold')
+        time.sleep(1)
