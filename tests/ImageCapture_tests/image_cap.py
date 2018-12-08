b763b07a6b0cbb75b8de0f89385afcc034efa410
cacowger <43860806+cacowger@users.noreply.github.com>
2018-12-08 14:08:36 -0500

    Update image_cap.py
    clean up the image caputre script for input into main script

--- tests/ImageCapture_tests/image_cap.py
+++ tests/ImageCapture_tests/image_cap.py
@@ -10,16 +10,13 @@ import pyexiv2
 import datetime
 from dronekit import *
 vehicle = connect('/dev/ttyS0', wait_ready=False, baud=921600)
-#from dronekit import connect, VehicleMode
-#datetime.now().strftime("%m_%d_%Y %H:%M:%S")
 
 
 def image_cap():
-	cap = cv2.VideoCapture(0) 							#zero is first webcam attatched
-	filename = ''
-	if cap.isOpened(): 								#checks if webcam is up and running
-		ret, frame = cap.read() 						#capture single frame from the webcam to read
-		#cv2.imshow('frame',frame)
+	cap = cv2.VideoCapture(0) #zero is first webcam attatched
+	filename = '' #redefines the filename as an open string
+	if cap.isOpened(): 	#checks if webcam is up and running
+		ret, frame = cap.read() #capture single frame from the webcam to read
 	else:
 		ret = False
 
@@ -31,31 +28,25 @@ def image_cap():
 		pyexiv2 is a module that allows your python scripts to read and write data
 		embedded in image files
 		'''
-		#%m_%d_%Y - month_day_year
-		#filename = "GrainImage_.png" #+ str(now) + ".png"
-		date=datetime.datetime.utcnow().strftime('%Y-%m-%d-%H-%M-%S')
+		date = datetime.datetime.utcnow().strftime('%Y-%m-%d-%H-%M-%S') #sets date to be current date/time in UTC
 		filename = 'GrainImage_%s.png'%date #writes filename with UTC date & time
-		#date=datetime.datetime.utcnow().strftime('%Y-%m-%d-%H-%M-%S')
-		path = "/home/pi/Grain_images/"
-		cv2.imwrite(os.path.join(path, filename), frame)
-		print(filename) #sanity check to make sure filename is storing properly
-		#cv2.imwrite(filename, frame) #writes the frame to an image file
-		#filename+=new_filename
+		path = "/home/pi/Grain_images/" # path file name to save to new directory on pi
+		cv2.imwrite(os.path.join(path, filename), frame) #writes the frame to the designated path with the desired filename
+		#print(filename) #sanity check to make sure filename is storing properly
 		metadata = pyexiv2.ImageMetadata('/home/pi/Grain_images/'+filename) #calls for the metadata off of the image
 		metadata.read() #reads the metadata
-		metadata.modified = True
-		metadata.writable = os.access('/home/pi/Grainimages/'+filename, os.W_OK)
+		metadata.modified = True #checks to see if the metadata can be modified
+		metadata.writable = os.access('/home/pi/Grainimages/'+filename, os.W_OK) #makes sure the file in the defined path is readable for metadata
 		key = 'Exif.Image.ImageDescription' #reference for saving the gps data in the exif tag
-		lat = (vehicle.location.global_frame.lat)
-		lon = (vehicle.location.global_frame.lon)
-		alt = (vehicle.location.global_frame.alt)
-		value = '(%s, %s, %s)' %(lat,lon,alt) #takes the gps data from dronekit
-		metadata[key] = pyexiv2.ExifTag(key, value) #writes the key and value to the exif tag
-		metadata.write()
+		lat = (vehicle.location.global_frame.lat) #sets lat to be the rover's latitude from dronekit
+		lon = (vehicle.location.global_frame.lon) #sets Lon to be the rover's Longitude from dronekit
+		alt = (vehicle.location.global_frame.alt) #sets Alt to be the rover's Altitude from dronekit
+		value = '(%s, %s, %s)' %(lat,lon,alt) #takes Lat, Lon, and Alt and sets it to be the exif tag value
+		metadata[key] = pyexiv2.ExifTag(key, value) #sets the key and value to the exif tag
+		metadata.write() #writes the new tag to the metadata
 
 	cap.release() #relases the camera function
 
 
 if __name__ == "__main__":
 	image_cap()
-	#main_2()
