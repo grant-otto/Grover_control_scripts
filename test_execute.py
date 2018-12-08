3449df023f7edc0e3a7b8b20c7ba3bda608c2dcc
seaotto <gotto@udel.edu>
2018-12-08 14:17:34 -0500

    update

--- test_execute.py
+++ test_execute.py
@@ -1,6 +1,11 @@
+#! /usr/bin/python
+
 #import image_cap.py
 import os
 
 def execute():
-    #exec(open("c:/Users/Courtney/Documents/GitHub/Grover_control_scripts/tests/ImageCapture_tests/image_cap.py").read())
-    execfile("python3 c:/Users/Courtney/Documents/GitHub/Grover_control_scripts/tests/ImageCapture_tests/image_cap.py")
+	exec(open("c:/Users/Courtney/Documents/GitHub/Grover_control_scripts/tests/ImageCapture_tests/image_cap.py").read())
+	#execfile("python3 c:/Users/Courtney/Documents/GitHub/Grover_control_scripts/tests/ImageCapture_tests/image_cap.py")
+
+if "_name_" == "_main_":
+	execute()
