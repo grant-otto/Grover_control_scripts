4d19ab12be6d5891cf13cd2b78c6d885a46f61de
Grant Otto <gotto@udel.edu>
2018-12-08 15:48:07 -0500

    updated image capture code

--- Grover_main_v2.py
+++ Grover_main_v2.py
@@ -67,11 +67,10 @@ def image_save():
 	It saves it to a datestamped filename, and adds the GPS information as text to the exif tag.
 	'''
 	#######	
-	cap = cv2.VideoCapture(0) 						#zero is first webcam attatched
-	filename = ''
-	if cap.isOpened(): 								#checks if webcam is up and running
-		ret, frame = cap.read() 					#capture single frame from the webcam to read
-		#cv2.imshow('frame',frame)
+	cap = cv2.VideoCapture(0) #zero is first webcam attatched
+	filename = '' #redefines the filename as an open string
+	if cap.isOpened(): 	#checks if webcam is up and running
+		ret, frame = cap.read() #capture single frame from the webcam to read
 	else:
 		ret = False
 
@@ -83,27 +82,24 @@ def image_save():
 		pyexiv2 is a module that allows your python scripts to read and write data
 		embedded in image files
 		'''
-		#%m_%d_%Y - month_day_year
-		#filename = "GrainImage_.png" #+ str(now) + ".png"
-		filename = 'GrainImage_%s.png'%datetime.datetime.utcnow().strftime('%Y-%m-%d-%H-%M-%S') #writes filename with UTC date & time
-		#path = 'C:/Users/Courtney/Documents/Github/Grover_control_scripts'
-		#cv2.imwrite(os.path.join(path, filename), frame)
-		print(filename) 									#sanity check to make sure filename is storing properly
-		cv2.imwrite(filename, frame) 						#writes the frame to an image file
-		#filename+=new_filename
-		metadata = pyexiv2.ImageMetadata(filename) 			#calls for the metadata off of the image
-		metadata.read() 									#reads the metadata
-		metadata.modified = True
-		metadata.writable = os.access(filename, os.W_OK)
-		key = 'Exif.Image.ImageDescription' 				#reference for saving the gps data in the exif tag
-		lat = (vehicle.location.global_frame.lat)
-		lon = (vehicle.location.global_frame.lon)
-		alt = (vehicle.location.global_frame.alt)
-		value = '(%s, %s, %s)' %(lat,lon,alt) 				#takes the gps data from dronekit
-		metadata[key] = pyexiv2.ExifTag(key, value) 		#writes the key and value to the exif tag
-		metadata.write()
+		date = datetime.datetime.utcnow().strftime('%Y-%m-%d-%H-%M-%S') 			# sets date to be current date/time in UTC
+		filename = 'GrainImage_%s.png'%date 										# writes filename with UTC date & time
+		path = "/home/pi/Grain_images/" 											# path file name to save to new directory on pi
+		cv2.imwrite(os.path.join(path, filename), frame) 							# writes the frame to the designated path with the desired filename
+		#print(filename) 															# sanity check to make sure filename is storing properly
+		metadata = pyexiv2.ImageMetadata('/home/pi/Grain_images/'+filename) 		# calls for the metadata off of the image
+		metadata.read() 															# reads the metadata
+		metadata.modified = True 													# checks to see if the metadata can be modified
+		metadata.writable = os.access('/home/pi/Grainimages/'+filename, os.W_OK) 	# makes sure the file in the defined path is readable for metadata
+		key = 'Exif.Image.ImageDescription' 										# reference for saving the gps data in the exif tag
+		lat = (vehicle.location.global_frame.lat) 									# sets lat to be the rover's latitude from dronekit
+		lon = (vehicle.location.global_frame.lon) 									# sets Lon to be the rover's Longitude from dronekit
+		alt = (vehicle.location.global_frame.alt) 									# sets Alt to be the rover's Altitude from dronekit
+		value = '(%s, %s, %s)' %(lat,lon,alt) 										# takes Lat, Lon, and Alt and sets it to be the exif tag value
+		metadata[key] = pyexiv2.ExifTag(key, value)									# sets the key and value to the exif tag
+		metadata.write() 															# writes the new tag to the metadata
 
-	cap.release() 											#relases the camera function
+	cap.release() 																	# relases the camera function
 
 if __name__ == "__main__":
 	image_save()
@@ -119,7 +115,7 @@ def distance_to_current_waypoint():
 	nextwaypoint = vehicle.commands.next
     if nextwaypoint==0:
         return None
-    missionitem=vehicle.commands[nextwaypoint-1] 	#commands are zero indexed
+    missionitem=vehicle.commands[nextwaypoint-1]									# commands are zero indexed
     lat = missionitem.x
     lon = missionitem.y
     alt = missionitem.z
@@ -140,12 +136,12 @@ def stepperdown():
 	'''
 	lowers stepper until the switch is closed
 	'''
-	control_pins = [7,11,13,15]     #Defines control pins
+	control_pins = [7,11,13,15]     												# Defines control pins
 
     for pin in control_pins:
         #print ("Setup pins")
-        GPIO.setup(pin, GPIO.OUT)   #Sets each control pin as an output
-        GPIO.output(pin,0)          # possibly set to GPIO.HIGH instead of 0
+        GPIO.setup(pin, GPIO.OUT)   												# Sets each control pin as an output
+        GPIO.output(pin,0)         													# possibly set to GPIO.HIGH instead of 0
 
     lower_seq = [
         [1,0,0,1],
@@ -179,12 +175,12 @@ def stepperup(cycles):
 	'''
 	raises stepper one cycle more than the number of cycles it went down to lock it in place
 	'''
-	control_pins = [7,11,13,15]     #Defines control pins
+	control_pins = [7,11,13,15]    								 					#Defines control pins
 
     for pin in control_pins:
         #print ("Setup pins")
-        GPIO.setup(pin, GPIO.OUT)   #Sets each control pin as an output
-        GPIO.output(pin,0)          # possibly set to GPIO.HIGH instead of 0
+        GPIO.setup(pin, GPIO.OUT)   												# Sets each control pin as an output
+        GPIO.output(pin,0)          												# possibly set to GPIO.HIGH instead of 0
 
     raise_seq = [
         [1,0,0,0],
@@ -266,40 +262,40 @@ def take_image():
 ''' MAIN LOOP '''
 #######
 
-while True: 											# starts a perpetual loop any time the vehicle is connected
-	while vehicle.mode==VehicleMode("AUTO"): 			# if the vehicle is in auto
+while True: 																		# starts a perpetual loop any time the vehicle is connected
+	while vehicle.mode==VehicleMode("AUTO"): 										# if the vehicle is in auto
 		print('AUTO')	
 		dist=distance_to_current_waypoint()
-		while dist>1 and not dist == None:				# if the vehicle is more than a meter away from the current WP
+		while dist>1 and not dist == None:											# if the vehicle is more than a meter away from the current WP
 			print('travelling to next waypoint...')
-			print('distance to next waypoint: %f', dist)# wait and display the distance to the next WP
+			print('distance to next waypoint: %f', dist)							# wait and display the distance to the next WP
 			time.sleep(1)
-			dist=distance_to_current_waypoint()			# reset the distance to the next WP and do again
-		if dist<1 and not dist == None: 				# if the vehicle is less than a meter away from the current WP
-			vehicle.mode = VehicleMode("HOLD") 			# put it on hold (on a rover, will stop the vehicle)
-			time.sleep(3) 								# wait for the vehicle to come to a stop (3 seconds)
+			dist=distance_to_current_waypoint()										# reset the distance to the next WP and do again
+		if dist<1 and not dist == None: 											# if the vehicle is less than a meter away from the current WP
+			vehicle.mode = VehicleMode("HOLD") 										# put it on hold (on a rover, will stop the vehicle)
+			time.sleep(3) 															# wait for the vehicle to come to a stop (3 seconds)
 			print(vehicle.mode)
 			print('take image')
 			take_image()
-			vehicle.mode = VehicleMode("AUTO") 			# put it back in AUTO
+			vehicle.mode = VehicleMode("AUTO") 										# put it back in AUTO
 			print(vehicle.mode)
-			time.sleep(15) 								# wait for 15 seconds so the vehicle can exit
+			time.sleep(15) 															# wait for 15 seconds so the vehicle can exit
 		elif dist==None:
 			print('At Home')
 			time.sleep(1)
-	while vehicle.mode==VehicleMode("MANUAL"): 			# if the vehicle is in MANUAL (remotely operated) mode:
+	while vehicle.mode==VehicleMode("MANUAL"): 										# if the vehicle is in MANUAL (remotely operated) mode:
 		#print('MANUAL')
-		if vehicle.channels['5'] < 1750: 				# if the switch by the H button on the Lightbridge is lowered
-			vehicle.mode = VehicleMode("HOLD") 			# put the vehicle in HOLD (on a rover, will stop the vehicle)
+		if vehicle.channels['5'] < 1750: 											# if the switch by the H button on the Lightbridge is lowered
+			vehicle.mode = VehicleMode("HOLD") 										# put the vehicle in HOLD (on a rover, will stop the vehicle)
 			time.sleep(3)
-			print(vehicle.mode) 						# wait for the vehicle to come to a stop
-			take_image()								# take the image
-			vehicle.mode = VehicleMode("MANUAL")		# put it back in manual
-			time.sleep(3)								# wait a bit for the mode to change
+			print(vehicle.mode) 													# wait for the vehicle to come to a stop
+			take_image()															# take the image
+			vehicle.mode = VehicleMode("MANUAL")									# put it back in manual
+			time.sleep(3)															# wait a bit for the mode to change
 			print(vehicle.mode)
 		time.sleep(.5)
 								# ***make sure the switch is immediately put back up after turning down
-	while vehicle.mode==VehicleMode("HOLD"):			# if the vehicle is in hold, just chill and tell the computer you're holding
+	while vehicle.mode==VehicleMode("HOLD"):										# if the vehicle is in hold, just chill and tell the computer you're holding
 		print('holding')
 		time.sleep(1)
 
