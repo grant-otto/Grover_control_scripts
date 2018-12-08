f12e31bb9962b04bd64c406a8f2ae2363dce8b41
Grant Otto <gotto@udel.edu>
2018-12-08 16:46:08 -0500

    added image test script

---
+++ tests/Grover_main_addimage.py
@@ -0,0 +1,121 @@
+#! /usr/bin/python
+'''
+Grover: a grain size-measuring rover using Pixhawk 2.0 connected via a serial connecti$
+
+Developed by Grant Otto, Courtney Cowger, Zach El-Azom, Eriq Gloria, Cole Stinger, Aar$
+University of Delaware Department of Mechanical Engineering
+Faculty Advisor: Adam Wickenheiser
+
+Developed for use at the Robotics Discovery Lab, University of Delaware School of Mari$
+Sponsor and PI of Robotics Discovery Lab: Arthur Trembanis
+
+wwww.udel.edu
+
+All code is public and free to use.
+'''
+
+'''
+***** KNOWN ISSUES *****
+- take image function needs to add in all code for taking the image, gps point, stepper control, etc
+- if the switch for the image capture is left down, the vehicle will continuously take image after image.
+	In the future, the r/c channel will be changed to a button but this will require a bit more troubleshooting
+	on the controller.
+
+'''
+
+from dronekit import *
+import subprocess
+vehicle = connect('/dev/ttyS0', wait_ready=False, baud=921600)
+print("Hello, my name is Grover. The current firmware version is: ")
+print(vehicle.version)
+print(vehicle.system_status.state)
+print(vehicle.armed)
+print(vehicle.mode)
+print('done checks')
+
+
+def take_image():
+	'''
+	controls the stepper motor going down, the image being taken,
+	and the stepper coming back up
+	'''
+	print('take image')
+    image_save()
+	'''
+	from whiteboard code:
+	while switch = LOW
+                lower stepper
+        stop stepper
+        retrieve gps point
+        take usb image
+        modify exif tag to include gps point
+        save to directory
+        raise stepper #may need a switch for the top of the stepper
+
+	'''
+def image_save():
+    subprocess.call("./home/pi/Grover_control_scripts/tests/ImageCapture_tests/image_cap.py")
+
+def distance_to_current_waypoint():
+    """
+    Gets distance in metres to the current waypoint. 
+    It returns None for the first waypoint (Home location).
+    """
+    nextwaypoint = vehicle.commands.next
+    if nextwaypoint==0:
+        return None
+    missionitem=vehicle.commands[nextwaypoint-1] #commands are zero indexed
+    lat = missionitem.x
+    lon = missionitem.y
+    alt = missionitem.z
+    targetWaypointLocation = LocationGlobalRelative(lat,lon,alt)
+    distancetopoint = get_distance_metres(vehicle.location.global_frame, targetWaypointLocation)
+    return distancetopoint
+
+distancetopoint=distance_to_current_waypoint()
+print('distance to waypoint: ', distancetopoint)
+
+
+
+
+while True: 							# starts a perpetual loop any time the vehicle is connected
+	while vehicle.mode==VehicleMode("AUTO"): 	# if the vehicle is in auto
+		print('AUTO')	
+		dist=distance_to_current_waypoint()
+		while dist>1 and not dist == None:
+			print('travelling to next waypoint...')
+			print('distance to next waypoint: %f', dist)
+			time.sleep(1)
+			dist=distance_to_current_waypoint()	
+		if dist<1 and not dist == None: 		# if the vehicle is less than a meter away from the current WP
+			vehicle.mode = VehicleMode("HOLD") 	# put it on hold (on a rover, will stop the vehicle)
+			time.sleep(3) 				# wait for the vehicle to come to a stop (3 seconds)
+			print(vehicle.mode)
+			print('take image')
+			#take_image()
+			vehicle.mode = VehicleMode("AUTO") 	# put it back in AUTO
+			print(vehicle.mode)
+			time.sleep(15) 				# wait for 15 seconds so the vehicle can exit
+		elif dist==None:
+			print('At Home')
+			time.sleep(1)
+	while vehicle.mode==VehicleMode("MANUAL"): 		# if the vehicle is in MANUAL (remotely operated) mode:
+		#print('MANUAL')
+		if vehicle.channels['5'] < 1750: 			# if the switch by the H button on the Lightbridge is lowered
+			vehicle.mode = VehicleMode("HOLD") 	# put the vehicle in HOLD (on a rover, will stop the vehicle)
+			time.sleep(3)
+			print(vehicle.mode) 				# wait for the vehicle to come to a stop
+			take_image()
+			vehicle.mode = VehicleMode("MANUAL")
+			time.sleep(3)
+			print(vehicle.mode)
+		time.sleep(.5)
+								# ***make sure the switch is immediately put back up after turning down
+	while vehicle.mode==VehicleMode("HOLD"):
+		print('holding')
+		time.sleep(1)
+
+
+
+
+
