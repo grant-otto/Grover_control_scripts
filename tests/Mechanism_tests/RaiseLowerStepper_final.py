e795f422c4b72fa0d28f89d16324f0dcaf08dc4a
seaotto <gotto@udel.edu>
2018-12-09 12:02:56 -0500

    finalized raise lower stepper code

--- tests/Mechanism_tests/RaiseLowerStepper_final.py
+++ tests/Mechanism_tests/RaiseLowerStepper_final.py
@@ -8,12 +8,18 @@ Test script for final version of stepper raising and lowering that is implemente
 
 import RPi.GPIO as GPIO
 import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(40,GPIO.OUT) #Pin 40 is the GPIO out, can be easily changed
+switchpin=33
+GPIO.setup(switchpin, GPIO.IN,pull_up_down=GPIO.PUD_UP)
+pwm = GPIO.PWM(40,50)
+pwm.start(0)
 
 def stepperdown():
-	'''
-	lowers stepper until the switch is closed
-	'''
-	control_pins = [7,11,13,15]     												# Defines control pins
+    '''
+    lowers stepper until the switch is closed
+    '''
+    control_pins = [7,11,13,15]     												# Defines control pins
 
     for pin in control_pins:
         #print ("Setup pins")
@@ -33,7 +39,7 @@ def stepperdown():
     cycles=0
     while True:
         if GPIO.input(switchpin)==True:
-            break()
+            break
         #Go through the sequence once. 1 rev = 8 cycles, gear reduction = 1/64, (8)(64) = 512 cycles
         #One revolution is 512 cycles = default
         for halfstep in range(8):
@@ -43,16 +49,15 @@ def stepperdown():
                 GPIO.output(control_pins[pin], (-1)*lower_seq[halfstep][pin])
                 #Initially, halfstep=0 & pin=0, so control_pins[pin] = 7 and halfstep_seq[halfstep][pin] = 1
             time.sleep(0.001)
-        i+=1
         cycles+=1
     return(cycles)
 
 
 def stepperup(cycles):
-	'''
-	raises stepper one cycle more than the number of cycles it went down to lock it in place
-	'''
-	control_pins = [7,11,13,15]    								 					#Defines control pins
+    '''
+    raises stepper one cycle more than the number of cycles it went down to lock it in place
+    '''
+    control_pins = [7,11,13,15]    								 					#Defines control pins
 
     for pin in control_pins:
         #print ("Setup pins")
