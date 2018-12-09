3384c3b22cbd321880cacf07648c1d1caa3ce5df
seaotto <gotto@udel.edu>
2018-12-09 11:43:24 -0500

    finalized wiper

--- tests/Mechanism_tests/wipe_final.py
+++ tests/Mechanism_tests/wipe_final.py
@@ -8,6 +8,7 @@ Test script for final version of wiper code that is implemented in Grover_main_v
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
 GPIO.setup(40,GPIO.OUT) #Pin 40 is the GPIO out, can be easily changed
 pwm = GPIO.PWM(40,50)
 pwm.start(0)
@@ -19,7 +20,7 @@ def SetAngle(ang):
     duty = ang/18 + 2
     GPIO.output(40,True)
     pwm.ChangeDutyCycle(duty)
-    sleep(1)
+    time.sleep(1)
     GPIO.output(40,False)
     pwm.ChangeDutyCycle(0)
 
@@ -28,11 +29,7 @@ def wipe():
     #######
     ''' this function controls the servo to wipe the lens 5 times'''
     ######
-    SetAngle(30)
-    SetAngle(180)
-
-
+    for i in range(5):
+        SetAngle(180)
+        SetAngle(30)
 wipe()
-time.sleep(5)
-wipe()
-print('done')
